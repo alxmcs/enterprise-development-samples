fc1cafb259e4673f49da1a7f4531daf9a0473afd
alxmcs <70561974+alxmcs@users.noreply.github.com>
2025-08-11 14:59:21 +0400

    inmemory repo added

---
+++ BookStore.Infrastructure.InMemory/BookInMemoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using BookStore.Domain.Data;
+using BookStore.Domain.Model.Books;
+
+namespace BookStore.Infrastructure.InMemory;
+
+/// <summary>
+/// Имплементация репозитория для книг
+/// </summary>
+public class BookInMemoryRepository : IRepository<Book, int>
+{
+    private List<Book> _books;
+    public BookInMemoryRepository()
+    {
+        _books = DataSeeder.Books;
+    }
+
+    public void Create(Book entity)
+    {
+        _books.Add(entity);
+    }
+
+    public void Delete(int entityId)
+    {
+        var author = Read(entityId);
+        if (author != null)
+            _books.Remove(author);
+    }
+
+    public Book Read(int entityId)
+    {
+        return _books.First(b => b.Id == entityId);
+    }
+
+    public List<Book> ReadAll()
+    {
+        return [.. _books];
+    }
+
+    public void Update(Book entity)
+    {
+        Delete(entity.Id);
+        Create(entity);
+    }
+}
