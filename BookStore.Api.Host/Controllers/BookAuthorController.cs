e204335cfb8d40b5b4be2b85395d9bd12d7aac38
alxmcs <70561974+alxmcs@users.noreply.github.com>
2025-10-17 17:28:26 +0400

    controllers updated

--- BookStore.Api.Host/Controllers/BookAuthorController.cs
+++ BookStore.Api.Host/Controllers/BookAuthorController.cs
@@ -2,6 +2,10 @@
 using BookStore.Application.Contracts.BookAuthors;
 
 namespace BookStore.Api.Host.Controllers;
-
+/// <summary>
+/// Контроллер для CRUD-операций над связями авторов и книг
+/// </summary>
+/// <param name="crudService">Аппликейшен служба связей</param>
+/// <param name="logger">Логгер</param>
 public class BookAuthorController(IApplicationService<BookAuthorDto, BookAuthorCreateUpdateDto, int> crudService, ILogger<BookAuthorController> logger)
     : CrudControllerBase<BookAuthorDto, BookAuthorCreateUpdateDto, int>(crudService, logger);
