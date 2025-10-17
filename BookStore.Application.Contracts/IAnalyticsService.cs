39d1b36624ef2cf8f3adf411922d4221cc077845
alxmcs <70561974+alxmcs@users.noreply.github.com>
2025-10-17 17:28:50 +0400

    contracts updated

---
+++ BookStore.Application.Contracts/IAnalyticsService.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Application.Contracts.Books;
+
+namespace BookStore.Application.Contracts;
+/// <summary>
+/// Служба для выполнения аналитических запросов
+/// </summary>
+public interface IAnalyticsService
+{
+    /// <summary>
+    /// Получает последние 5 книг выбранного автора
+    /// </summary>
+    /// <param name="dtoId">Идентификатор автора</param>
+    /// <returns>Список книг</returns>
+    List<BookDto> GetLast5AuthorsBook(int dtoId);
+
+    /// <summary>
+    /// Получает топ 5 авторов по числу написанных страниц
+    /// </summary>
+    /// <returns>Список кортежей вида (имя автора, число страниц)</returns>
+    List<KeyValuePair<string, int?>> GetTop5AuthorsByPageCount();
+}
