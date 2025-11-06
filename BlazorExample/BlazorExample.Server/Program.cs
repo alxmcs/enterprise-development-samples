ba30dfe7abab7480505109495289102e4835dab6
alxmcs <70561974+alxmcs@users.noreply.github.com>
2025-11-06 23:01:10 +0400

    examples added

---
+++ BlazorExample/BlazorExample.Server/Program.cs
@@ -0,0 +1,28 @@
+using BlazorExample.Wasm.Components;
+using BlazorExample.Server.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents()
+	.AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(TimerComponent).Assembly);
+
+app.Run();
+
---
+++ BlazorExample/Winx.Wasm/Domain/Fairy.cs
@@ -0,0 +1,22 @@
+﻿namespace Winx.Wasm.Domain;
+
+/// <summary>
+/// Доменная сущность феи винкс
+/// </summary>
+public class Fairy
+{
+    /// <summary>
+    /// Идентификатор феи винкс
+    /// </summary>
+    public int? Id { get; set; }
+
+    /// <summary>
+    /// Отображаемое имя феи винкс
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Ссылка на изображение феи винкс
+    /// </summary>
+    public Uri? PhotoUrl { get; set; }
+}
